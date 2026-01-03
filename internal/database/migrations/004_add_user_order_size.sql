a9494fc3b593cafc296747424d959a32e7cf31df
Virhan <virhannn@gmail.com>
2026-01-03 08:55:58 +0700

    chore: change default fixed_order_size for users from 30.0 to 1.0.

--- internal/database/migrations/004_add_user_order_size.sql
+++ internal/database/migrations/004_add_user_order_size.sql
@@ -1,2 +1,2 @@
 -- Add fixed_order_size to users table
-ALTER TABLE users ADD COLUMN fixed_order_size DECIMAL(20, 8) DEFAULT 30.0;
+ALTER TABLE users ADD COLUMN fixed_order_size DECIMAL(20, 8) DEFAULT 1.0;
